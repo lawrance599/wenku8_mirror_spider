ef7a7682ffdf61b243d3584b22be3c41915a6ce8
fan_yixin <fanyixin050909@outlook.com>
2025-03-01 15:57:18 +0800

    feat(main): - 新增了main爬虫，集成了之前所有爬虫的功能， 目前关闭了其他爬虫的入口，但目前不做删除处理

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 import os
 from scrapy import cmdline as cmd
 if __name__ == '__main__':
-    print("爬虫列表：")
-    os.system("scrapy list")
-    spider = input("请输入爬虫名：").strip()
-    os.system(f"nohup scrapy crawl {spider} > /dev/null & echo $! > run.pid")
+    os.system(f"nohup scrapy crawl main > /dev/null & echo $! > run.pid")
     with open("run.pid") as f:
         pid = f.read()
-    print(f"爬虫{spider}已启动， pid为{pid}")
+    print(f"爬虫已启动， pid为{pid}")
